8dddae3dbae771f4c31a444a6ff9b4262e3bfee0
Ranjeet Singh <techonerd1.0@gmail.com>
2021-10-05 22:03:39 +0530

    'bank' module not found fix

--- Bank_api/urls.py
+++ Bank_api/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from bank.api import views
+# from bank.api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',views.Banklist.as_view()),
-    path('api/list/',views.BankListView.as_view(),name='list')
+    # path('api/',views.Banklist.as_view()),
+    # path('api/list/',views.BankListView.as_view(),name='list')
 ]
