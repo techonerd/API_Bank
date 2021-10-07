1bfd9383d20b91a549bd18c2ee773ba677f2b75d
Ranjeet Singh <22074809+techonerd@users.noreply.github.com>
2021-10-07 23:27:04 +0530

    Secret variables and API initialize (#13)
    * CI implementation improve
    
    * API app for bank init
    
    * create serializers class for bank
    
    * exclude migrations files
    
    * use of env variables
    
    * CI test with secrets variable
    
    * CI test with secrets variable
    
    * CI test with env variable
    
    * install bank app

--- Bank_api/settings.py
+++ Bank_api/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(av65i2z-hf9l&h9)a%n_$0b26!ep&47(9c7^zf6*^j+5pxegd'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = []
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'bank',
+    'bank.apps.BankConfig',
     'rest_framework',
 ]
 
---
+++ bank/api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from bank.models import Bank
+
+class BankSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Bank
