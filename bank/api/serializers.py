c2a225f4a80b4efc8d0474fe82c5bd513f70f02e
Amit Singh <amitjordanthegr8@gmail.com>
2021-10-09 13:44:13 +0530

    Revert "Development" (#15)

--- bank/api/serializers.py
+++ bank/api/serializers.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 from bank.models import Bank
 
 class BankSerializer(serializers.ModelSerializer):
-
+    
     class Meta:
         model = Bank
-        # can't create ModelSerializers without fields
-        fields = '__all__'
--- bank/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from bank.api.views import BankListView, Banklist
-
-app_name = 'bank'
-
-urlpatterns = [
-    path('bank/', Banklist, name='bank-list'),
-    path('bank/list/',BankListView.as_view(), name="list") # paginated response
-]
